0ab731d037d7b1e5091ab71e332f79c93d79cac3
coffeewar <coffeewar@163.com>
2016-05-20 14:11:13 +0800

    命令路由更改

--- enode/src/main/java/com/qianzhui/enode/rocketmq/command/CommandKeyProvider.java
+++ enode/src/main/java/com/qianzhui/enode/rocketmq/command/CommandKeyProvider.java
@@ -1,6 +1,5 @@
 package com.qianzhui.enode.rocketmq.command;
 
-import com.alibaba.rocketmq.common.message.MessageConst;
 import com.qianzhui.enode.commanding.ICommand;
 import com.qianzhui.enode.commanding.ICommandKeyProvider;
 
@@ -10,6 +9,7 @@ import com.qianzhui.enode.commanding.ICommandKeyProvider;
 public class CommandKeyProvider implements ICommandKeyProvider {
     @Override
     public String getKey(ICommand command) {
-        return command.getAggregateRootId() == null ? command.id() : command.id() + MessageConst.KEY_SEPARATOR + command.getAggregateRootId();
+//        return command.getAggregateRootId() == null ? command.id() : command.id() + MessageConst.KEY_SEPARATOR + command.getAggregateRootId();
+        return command.getAggregateRootId() == null ? command.id() : command.getAggregateRootId();
     }
 }
