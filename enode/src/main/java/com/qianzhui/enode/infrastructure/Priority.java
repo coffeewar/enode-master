24faffab341aa1fb0e39ff97ff043d31b0e758d8
xujunbo <coffeewar@163.com>
2018-01-18 10:58:12 +0800

    注解类增加RetentionPolicy.RUNTIME注解，解决优先级未生效bug.

--- enode/src/main/java/com/qianzhui/enode/infrastructure/Priority.java
+++ enode/src/main/java/com/qianzhui/enode/infrastructure/Priority.java
@@ -1,8 +1,12 @@
 package com.qianzhui.enode.infrastructure;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by junbo_xu on 2016/3/27.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Priority {
 
     int value() default 0;
